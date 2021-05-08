d3b3f4e21d005231304450f705968858b48f7c08
CDinuwan <chanukadinuwan35@gmail.com>
2021-05-08 22:19:09 +0530

    Finish unit testing

--- Test_calc.py
+++ Test_calc.py
@@ -1,8 +1,32 @@
 import unittest
 import Unit
 
+
 class TestUnit(unittest.TestCase):
 
-    def test_add(self):
-        result=Unit.add()
-        self.assertEqual()
+    def test_add(self):  # name of the method should be begin with test
+        self.assertEqual(Unit.add(10, 5), 15)
+        self.assertEqual(Unit.add(-1, 1), 0)
+        self.assertEqual(Unit.add(-1, -1), -2)
+
+    def test_subtract(self):  # name of the method should be begin with test
+        self.assertEqual(Unit.substract(10, 5), 5)
+        self.assertEqual(Unit.substract(-1, 1), -2)
+        self.assertEqual(Unit.substract(-1, -1), 0)
+
+    def test_multiply(self):  # name of the method should be begin with test
+        self.assertEqual(Unit.multiply(10, 5), 50)
+        self.assertEqual(Unit.multiply(-1, 1), -1)
+        self.assertEqual(Unit.multiply(-1, -1), 1)
+
+    def test_divide(self):  # name of the method should be begin with test
+        self.assertEqual(Unit.divide(10, 5), 2)
+        self.assertEqual(Unit.divide(-1, 1), -1)
+        self.assertEqual(Unit.divide(-1, -1), 1)
+
+        with self.assertRaises(ValueError):
+            Unit.divide(10, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- Unit.py
+++ Unit.py
@@ -1,15 +1,19 @@
-def add(x,y):
-    return x+y
+def add(x, y):
+    return x + y
 
-def substract(x,y):
-    return x-y
 
-def multiply(x,y):
-    return x*y
+def substract(x, y):
+    return x - y
 
-def divide(x,y):
-    if y==0:
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
         print("Cannot devide by zero")
-    return(x/y)
+    return x / y
 
-print(add(4,5))
+
+print(add(4, 5))
