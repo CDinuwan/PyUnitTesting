6503a8873f33d4a43d64ef9dda06880a5ab435b1
CDinuwan <chanukadinuwan35@gmail.com>
2021-05-08 20:21:09 +0530

    Unit testing

---
+++ Test_calc.py
@@ -0,0 +1,8 @@
+import unittest
+import Unit
+
+class TestUnit(unittest.TestCase):
+
+    def test_add(self):
+        result=Unit.add()
+        self.assertEqual()
---
+++ Unit.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    return x+y
+
+def substract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    if y==0:
+        print("Cannot devide by zero")
+    return(x/y)
+
+print(add(4,5))
